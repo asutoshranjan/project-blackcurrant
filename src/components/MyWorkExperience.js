d662ecb4a31f52cb68430478e228d0d2472c7a1b
Asutosh Ranjan <asutoshranjan.work@gmail.com>
2024-04-26 16:08:24 +0530

    biglynx update

--- src/components/MyWorkExperience.js
+++ src/components/MyWorkExperience.js
@@ -4,6 +4,7 @@ import Lottie from "react-lottie-player";
 import Reveal from "./animations/Reveal";
 import lottieJson from "../images/lottie-assets/computer-loop.json";
 import biglynx from "../images/biglynx_logo.png";
+import vqr from "../images/vqr-tech.png";
 import appwrite from "../images/appwrite_logo.png";
 import fractal from "../images/fractal_logo1.png";
 import freelance from "../images/freelance_logo_1.png";
@@ -53,41 +54,7 @@ export default function WorkExperience() {
         </div>
 
         <div className="mt-10 lg:mt-4 grid divide-x divide-y divide-gray-700 overflow-hidden rounded-3xl border text-gray-600 border-gray-700 sm:grid-cols-2 lg:grid-cols-4 lg:divide-y-0 xl:grid-cols-4">
-          <div className="group relative bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
-            <div className="relative space-y-8 py-12 p-8">
-              <img src={biglynx} className="h-12" alt="biglynx" />
-              <div className="space-y-2">
-                <h5 className="text-xl font-semibold text-white transition group-hover:text-secondary">
-                  Mobile Developer
-                </h5>
-                <p className="text-gray-300 text-sm">
-                  As a React Native app developer, I deliver pixel-perfect
-                  designs, manage end-to-end development, handle API
-                  integrations, and ensure Play Store releases
-                </p>
-              </div>
-              <a
-                href="https://www.biglynx.com/"
-                target="_blank"
-                className="flex items-center justify-between group-hover:text-secondary"
-              >
-                <span className="text-sm">biglynx.com</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-          <div className="group relative bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
+        <div className="group relative bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
             <div className="relative space-y-8 py-12 p-8">
               <img src={appwrite} className="h-12" alt="appwrite" />
               <div className="space-y-2">
@@ -121,6 +88,42 @@ export default function WorkExperience() {
               </a>
             </div>
           </div>
+          <div className="group relative bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
+            <div className="relative space-y-8 py-12 p-8">
+              <img src={vqr} className="h-12" alt="vqr-tech" />
+              <div className="space-y-2">
+                <h5 className="text-xl font-semibold text-white transition group-hover:text-secondary">
+                  Mobile Developer
+                </h5>
+                <p className="text-gray-300 text-sm">
+                  As a React Native app developer, I deliver pixel-perfect
+                  designs, manage end-to-end development, handle API
+                  integrations, and ensure Play Store releases
+                </p>
+              </div>
+              <a
+                href="https://vqrcode.vercel.app/"
+                target="_blank"
+                className="flex items-center justify-between group-hover:text-secondary"
+              >
+                {/* <span className="text-sm">biglynx.com</span> */}
+                <span className="text-sm">vqr.tech</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-5 h-5 -translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+          
           <div className="group relative bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
             <div className="relative space-y-8 py-12 p-8">
               <img src={fractal} className="h-12" alt="fractal" />
